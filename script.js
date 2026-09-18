a7d1137e42ef90f5f6289cf2b83d19315775706e
alfiansyah <achmadalfiansyah069@gmail.com>
2026-09-18 07:30:21 +0700

    feat: add menu item prices, contact form submission logic, and SEO metadata

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+const contactForm = document.querySelector(".contact-form");
+const formStatus = document.querySelector(".form-status");
+const submitButton = document.querySelector(".submit-button");
+
 
 menuOpenButton.addEventListener("click", () => {
   // Toggle mobile menu visibility
   document.body.classList.toggle("show-mobile-menu");
 });
 
-// Cloi
+// Close Mobile Menu when clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 
+// Close Mobile Menu when Link is clicked
 navLinks.forEach(link => {
   link.addEventListener("click", () => menuOpenButton.click());
+});
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  submitButton.innerText = "Sending...";
+  submitButton.disabled = true;
+
+  setTimeout(() => {
+    submitButton.innerText = "Send";
+    submitButton.disabled = false;
+    formStatus.textContent = "Thank you! Your message has been sent succesfully.";
+
+    formStatus.classList.add("success");
+    
+    contactForm.reset();
+    setTimeout(() => {
+      formStatus.classList.remove("success");
+      formStatus.textConten = "";
+    
+    }, 5000);
+
+
+  },800);
 });
 
 // Initialize Swiper
